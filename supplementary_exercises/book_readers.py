1ea02584e605191d0849aabdb5ba52734386e44a
Abhilash Vivek <abhilashvivek@gmail.com>
2023-08-25 23:41:03 +0530

    solution_supplementary_exercises

--- supplementary_exercises/book_readers.py
+++ supplementary_exercises/book_readers.py
@@ -7,3 +7,24 @@
 # 1) Open `war_and_peace.txt`, read the whole file content and store it in a variable
 # 2) Open `crime_and_punishment.txt` and overwrite the whole content with the last two words of the text of `war_and_peace.txt`
 # 3) Loop over all three files and print out only the first word of each.
+
+#TASK-1
+with open(r"supplementary_exercises\books\war_and_peace.txt", "r",encoding="utf-8") as the_file:
+    file_string = the_file.read()
+    
+#TASK-2
+temp_list = list(file_string.split(" "))        #converting into list to detect words.
+
+with open(r"supplementary_exercises\books\crime_and_punishment.txt", "w") as the_file:
+    the_file.write(temp_list[-2] + " " + temp_list[-1])
+    
+#TASK-3
+file_list = [r"supplementary_exercises\books\war_and_peace.txt" ,
+             r"supplementary_exercises\books\crime_and_punishment.txt" ,
+             r"supplementary_exercises\books\pride_and_prejudice.txt"]       #creating a list for iteration
+
+for path in file_list:
+    with open(path, "r",encoding="utf-8") as the_file:
+        file_string = the_file.read()
+        temp_list = list(file_string.split(" "))
+        print(f"The first word from '{path}' is:   '{temp_list[0]}'")
--- supplementary_exercises/csv_practice.py
+++ supplementary_exercises/csv_practice.py
@@ -2,3 +2,25 @@
 #Retrieve all names that start with an S
 #Add the ages of all the users print the total
 #Retrieve the phone number of the user Jay 
+import csv
+
+with open(r"supplementary_exercises\csv_practice_users.csv", 'r') as csv_file:      #opening file
+    csvreader = csv.reader(csv_file)
+    print("- The names starting with S are:",end=" ")
+    for row in csvreader:
+        if row[0].startswith("S") or row[0].startswith("s"):        #printng names starting with an S
+            print(row[0],end=" ")
+
+with open(r"supplementary_exercises\csv_practice_users.csv", 'r') as csv_file:      #opening file
+    csvreader = csv.reader(csv_file)    
+    total_age = 0           #declaring age variable type
+    for row in csvreader:
+        if row[1].isnumeric():
+            total_age += int(row[1])
+    print(f"\n- The total age is:  '{total_age}'")             #printing sum of all the ages
+    
+with open(r"supplementary_exercises\csv_practice_users.csv", 'r') as csv_file:      #opening file
+    csvreader = csv.reader(csv_file)    
+    for row in csvreader:
+        if row[0].startswith("Jay"):
+            print(f"- Jay's phone number is:  '{row[2]}'")
